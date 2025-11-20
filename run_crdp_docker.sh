3b4c0694a80867f11b9eb15f8c4033500e8d26c8
sjrhee <50762904+sjrhee@users.noreply.github.com>
2025-11-20 18:40:26 +0900

    Update run_crdp_docker.sh

--- run_crdp_docker.sh
+++ run_crdp_docker.sh
@@ -1,1 +1,2 @@
-docker run -d -e KEY_MANAGER_HOST=192.168.0.230 -e REGISTRATION_TOKEN=U3Sca0xJYr1QHXFWJGBA15uXGmMNnDdV0syeCOXt6LjXa8PYtbxyuyWmuL9P1IrA --restart unless-stopped -p 32082:8090 -p 32080:8080 -e SERVER_MODE=no-tls thalesciphertrust/ciphertrust-restful-data-protection
+docker run -d -e KEY_MANAGER_HOST=192.168.0.230 -e REGISTRATION_TOKEN=U3Sca0xJYr1QHXFWJGBA15uXGmMNnDdV0syeCOXt6LjXa8PYtbxyuyWmuL9P1IrA \
+--restart unless-stopped -p 32082:8090 -p 32080:8080 -e SERVER_MODE=no-tls thalesciphertrust/ciphertrust-restful-data-protection
